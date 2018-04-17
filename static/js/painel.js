1f82252cec43eb68dfcf09e78d8d95b2c0b738d6
Mauricio-vieira <mauriciohike1@gmail.com>
2018-04-17 10:57:35 -0300

    Estilização de botão "Go to top", e inserção do mesmo nas páginas

--- static/js/painel.js
+++ static/js/painel.js
@@ -82,3 +82,18 @@ $(function() {
         $('#calendar').fullCalendar('rerenderEvents');
     });
 });
+window.onscroll = function() {
+	scrollFunction()
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById("myBtn").style.display = "block";
+	} else {
+		document.getElementById("myBtn").style.display = "none";
+	}
+}
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}

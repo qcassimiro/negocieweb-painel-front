f05e4bf0046c34bfde135d5814c67c2129d11dfd
Mauricio-vieira <mauriciohike1@gmail.com>
2018-04-24 12:39:52 -0300

    Portugues nos campos de data

--- static/js/painel.js
+++ static/js/painel.js
@@ -108,3 +108,14 @@ $("#submit").click(function() {
         }
     });
 });
+function dataAtualFormatada(){
+    var data = new Date();
+    var dia = data.getDate();
+    if (dia.toString().length == 1)
+      dia = "0"+dia;
+    var mes = data.getMonth()+1;
+    if (mes.toString().length == 1)
+      mes = "0"+mes;
+    var ano = data.getFullYear();  
+    return dia+"/"+mes+"/"+ano;
+}

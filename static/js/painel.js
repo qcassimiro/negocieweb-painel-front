f38ecab9f6b3a8276b362a28ec57b987557f4e8f
qcassimiro <qcassimiro@gmail.com>
2018-04-25 08:28:45 -0300

    funcao esquisita retirada

--- static/js/painel.js
+++ static/js/painel.js
@@ -108,14 +108,3 @@ $("#submit").click(function() {
         }
     });
 });
-function dataAtualFormatada(){
-    var data = new Date();
-    var dia = data.getDate();
-    if (dia.toString().length == 1)
-      dia = "0"+dia;
-    var mes = data.getMonth()+1;
-    if (mes.toString().length == 1)
-      mes = "0"+mes;
-    var ano = data.getFullYear();  
-    return dia+"/"+mes+"/"+ano;
-}
